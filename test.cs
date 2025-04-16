3878b80652dfe501ba35f95ad25d71e554b06275
于显野 <147622013@qq.com>
2025-04-16 07:49:42 +0000

    init

---
+++ test.cs
@@ -0,0 +1,23 @@
+public class Test
+{
+    // Add your class members here
+
+
+    public  void TestMethod()
+    {
+        // Add your test code here
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static void Main(string[] args)
+    {
+        Test test = new Test();
+        test.TestMethod();
+    }
+    public void AnotherMethod()
+    {
+        // Another method
+        Console.WriteLine("This is another method.");
+    }
+
+}
